9f035c094eaa8798d4b85f1e6ef125d34dbbb5d4
Chris Proto <chris.proto@devopsgorilla.com>
2018-04-01 23:34:52 -0600

    Add protocol and URL to terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket" {
-  value = "${module.my-app.s3_bucket_id}"
+    value = "${module.my-app.s3_bucket_id}"
 }
 
 output "domain_name" {
-  value = "${module.my-app.domain_name}"
+    value = "http://${module.my-app.domain_name}/"
 }
